9026bccc1fc5362ab4ea03e4c19861cae3f72657
조수아 <josua@josuaui-MacBookPro.local>
2023-10-14 22:33:44 +0900

    fdsf

--- background_remove.py
+++
@@ -1,6 +0,0 @@
-from rembg import remove
-from PIL import Image
-
-input = Image.open('./result/test2Cut.jpg') # load image
-output = remove(input) # remove background
-output.save('rembg.png') # save image
